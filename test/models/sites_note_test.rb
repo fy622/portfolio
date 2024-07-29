f7f9911e3d360291eb5135521ad5113fe2186a67
fy622 <yuta.fumoto@gmail.com>
2024-07-29 14:17:42 +0900

    モデル作成

--- test/models/sites_note_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SitesNoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/sites_review_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SitesReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
