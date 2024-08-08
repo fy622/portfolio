fca20ae84a4b093fc8b56a395721e1d537238e64
fy622 <yuta.fumoto@gmail.com>
2024-08-08 19:09:20 +0900

    close #76 スマホで開発環境見る方法調査完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.web_console.whitelisted_ips = '10.18.227.198'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,5 @@ Rails.application.routes.draw do
   resources :favorite_sites, only: [:create, :destroy]
   resources :site_notes, only: [:create, :update]
   resources :site_reviews, only: [:create, :update, :destroy, :index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
 end
